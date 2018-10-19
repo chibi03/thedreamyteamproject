a5c40a20f004ce2244526006d2812550758c530c
Samirah Amadu <amadu@student.tugraz.at>
2018-10-20 00:42:28 +0200

    Initial version random via /dev/urandom

--- tls/random.cpp
+++ tls/random.cpp
@@ -1,7 +1,24 @@
 #include "random.h"
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+
+using std::ios;
 
 bool get_random_data(uint8_t* data, std::size_t size)
 {
-  /// \todo Fetch size random bytes and store them in data.
-  return false;
+  std::ifstream randomstream;
+  randomstream.open("/dev/urandom", ios::in);
+  if(randomstream.is_open()){
+      char * buffer = new char[size];
+      randomstream.read(buffer, size);
+      randomstream.close();
+
+      memcpy(data, buffer, size);
+  } else {
+      return false;
+  }
+
+  return true;
 }
