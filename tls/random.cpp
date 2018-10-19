2d4e4b515de514282ada9cae7aa650f63fb131aa
Samirah Amadu <amadu@student.tugraz.at>
2018-10-20 01:21:29 +0200

    Add comment

--- tls/random.cpp
+++ tls/random.cpp
@@ -14,7 +14,7 @@ bool get_random_data(uint8_t* data, std::size_t size)
 
     randomstream.open(filename, ios::in);
     if(randomstream.is_open()){
-        char * buffer = new char[size];
+        char * buffer = new char[size]; //HELP! memory leak!
         randomstream.read(buffer, size);
         randomstream.close();
 
