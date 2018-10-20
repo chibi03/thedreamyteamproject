d62b9cea091d2eabb0111a2572bf35f4ead35a5d
Samirah Amadu <amadu@student.tugraz.at>
2018-10-20 18:38:09 +0200

    Add check for size value

--- tls/random.cpp
+++ tls/random.cpp
@@ -9,22 +9,25 @@ using std::ios;
 
 bool get_random_data(uint8_t* data, std::size_t size)
 {
-    ifstream randomstream;
-    string filename = "/dev/urandom";
+    if (size) {
+        ifstream randomstream;
+        string filename = "/dev/urandom";
 
-    randomstream.open(filename, ios::in);
-    if(randomstream.is_open()){
-        char * buffer = new char[size]; //HELP! memory leak!
-        randomstream.read(buffer, size);
-        randomstream.close();
+        randomstream.open(filename, ios::in);
+        if (randomstream.is_open()) {
+            char *buffer = new char[size]; //HELP! memory leak!
+            randomstream.read(buffer, size);
+            randomstream.close();
 
-        memcpy(data, buffer, size);
+            memcpy(data, buffer, size);
+        }
+
+        if (randomstream.fail()) {
+            cout << "Failed to open " << filename << "! File may not exist." << endl;
+            return false;
+        }
+
+        return true;
     }
-
-    if (randomstream.fail()) {
-        cout << "Failed to open " + filename + "! File may not exist." << endl;
-        return false;
-    }
-
-    return true;
+    return false;
 }
