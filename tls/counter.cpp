941b61d51080efa26bbce6565c1d6a1876cc2a11
Samirah Amadu <amadu@student.tugraz.at>
2018-10-22 22:23:13 +0200

    Add comments to describe nonce

--- tls/counter.cpp
+++ tls/counter.cpp
@@ -3,29 +3,45 @@
 
 #include <string.h>
 
-
+/**
+ * Initialises the counter with the given data
+ */
 incrementing_nonce::incrementing_nonce(const std::vector<uint8_t>& bytes) {
 	for (unsigned int i = 0; i < bytes.size(); i++) {
+		// initialize IV and nonce
 		this->nonce_data.push_back(bytes[i]);
 		this->init_nonce_data.push_back(bytes[i]);
 	}
 	this->internal_counter = 0;
-
 }
 
+/**
+ * Increments the counter by one and generates a new nonce by XORing the counter and the IV.
+ */
 incrementing_nonce& incrementing_nonce::operator++() {
-	this->internal_counter++;
+	this->internal_counter++; // increment counter
+
 	uint64_t counter;
 	memcpy(&counter, this->init_nonce_data.data() + this->init_nonce_data.size() - 8, sizeof counter);
-	counter = counter ^ hton<uint64_t>(this->internal_counter);
+
+	counter = counter ^ hton<uint64_t>(this->internal_counter); //XOR counter with Big endian counter
 	memcpy(this->nonce_data.data() + this->nonce_data.size() - 8, &counter, sizeof counter);
-  return *this;
+
+	return *this;
 }
 
+/**
+ * Replaces the current nonce with the given data
+ * @param bytes
+ */
 void incrementing_nonce::reset(const std::vector<uint8_t>& bytes) {
 	memcpy(this->nonce_data.data(), bytes.data(), bytes.size());
 }
 
+/**
+ * Returns the current nonce value
+ * @return nonce value
+ */
 const std::vector<uint8_t> incrementing_nonce::nonce() {
 	std::vector<uint8_t> copy;
 	for(unsigned int i = 0; i < this->nonce_data.size(); i++) {
