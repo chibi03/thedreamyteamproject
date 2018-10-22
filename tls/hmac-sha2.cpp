8349d9562cbe6dfc8de88cd2828ca4fa918f3347
Samirah Amadu <amadu@student.tugraz.at>
2018-10-22 22:16:43 +0200

    Formatting

--- tls/hmac-sha2.cpp
+++ tls/hmac-sha2.cpp
@@ -1,28 +1,22 @@
 #include "hmac-sha2.h"
 
-hmac_sha2::hmac_sha2(const uint8_t* key, std::size_t keysize)
-{
-  /// \todo Initialze with given key.
-    uint8_t newKey [sha2::block_size];
+hmac_sha2::hmac_sha2(const uint8_t *key, std::size_t keysize) {
+    /// \todo Initialze with given key.
+    uint8_t newKey[sha2::block_size];
     sha2 head;
 
-	if(keysize > block_size)
-	{
-		head.update(key, keysize);
-		memcpy(newKey, head.digest().data(), digest_size);
+    if (keysize > block_size) {
+        head.update(key, keysize);
+        memcpy(newKey, head.digest().data(), digest_size);
         keysize = digest_size;
-	}
-	else
-	{
-		memcpy(newKey, key, keysize);
-	}
+    } else {
+        memcpy(newKey, key, keysize);
+    }
 
-    for(size_t i=keysize; i<block_size; ++i)
-    {
+    for (size_t i = keysize; i < block_size; ++i) {
         newKey[i] = 0x00;
     }
-    for(size_t i=0; i<block_size; ++i)
-    {
+    for (size_t i = 0; i < block_size; ++i) {
         opad[i] = newKey[i] ^ 0x5c;
         ipad[i] = newKey[i] ^ 0x36;
     }
@@ -30,19 +24,17 @@ hmac_sha2::hmac_sha2(const uint8_t* key, std::size_t keysize)
     header.update(ipad, block_size);
 }
 
-void hmac_sha2::update(const uint8_t* bytes, std::size_t size)
-{
-  /// \todo Feed data to HMAC.
-  header.update(bytes, size);
+void hmac_sha2::update(const uint8_t *bytes, std::size_t size) {
+    /// \todo Feed data to HMAC.
+    header.update(bytes, size);
 }
 
-hmac_sha2::digest_storage hmac_sha2::digest()
-{
-  /// \todo Finalize HMAC compuation and return computed digest.
-  sha2 head;
+hmac_sha2::digest_storage hmac_sha2::digest() {
+    /// \todo Finalize HMAC compuation and return computed digest.
+    sha2 head;
 
-  head.update(opad, block_size);
-  head.update(header.digest().data(), digest_size);
+    head.update(opad, block_size);
+    head.update(header.digest().data(), digest_size);
 
-  return head.digest();
+    return head.digest();
 }
