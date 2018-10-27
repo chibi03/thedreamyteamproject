e7d9aac1075d223a5cf293fe0bc9c11beafe32f2
Inti Mendoza <i.mendozaestrada@student.tugraz.at>
2018-10-27 23:03:07 +0200

    added skellington (doot doot) of aesgcm
    TODO: gmult
    TODO: proper block sizing
    TODO: check ghash in decrypt from ciphertext

--- tls/aes128gcm.cpp
+++ tls/aes128gcm.cpp
@@ -2,6 +2,8 @@
 #include "aes/aes128.h"
 #include <iostream>
 
+#include "random.h"
+
 // Take care to program the GCM specific functions in constant time,
 // meaning that no conditional branches or conditional loads that depend
 // on the key, the nonce or the data are allowed. This means that the
@@ -11,24 +13,78 @@
 
 aes128gcm::aes128gcm(){
 /// \todo Initialize with an all 0 key.
+  for(unsigned int i = 0; i < 16; i++) {
+    this->key.push_back(0);
+  }
+  get_random_data(this->random_data.data(), 32);
+  this->aes128 = aes128(this->key.data());
 }
 
 aes128gcm::aes128gcm(const key_storage& key)
 {
   /// \todo Initialise with the given key.
+  for(auto it = key.begin(); it != key.end(); it++) {
+    this->key.push_back(*it);
+  }
+  get_random_data(this->random_data.data(), 32); 
+  this->aes128 = aes128(this->key.data());
 }
 
 void aes128gcm::set_key(const key_storage& key)
 {
   /// \todo Reset the key.
+  uint8_t size_diff = this->key.size() - key.size();
+  for(unsigned int i = 0; i < size_diff; i++) {
+    this->key.push_back(0);
+  }
+  for(unsigned int i = 0; i < key.size(); i++) {
+    this->key[i] = key[i];
+  }
+  this->aes128.set_key(this->key.data());
+}
+
+void aes128gcm::gmult(std::vector<uint8_t>& tag, std::vector<uint8_t>& data) {
+  //TODO: GMULT the last element of the tag with the data and append to the tag vector
 }
 
 bool aes128gcm::encrypt(std::vector<uint8_t>& ciphertext, const std::vector<uint8_t>& plaintext,
                         const std::vector<uint8_t>& nonce_data,
                         const std::vector<uint8_t>& additional_data) const
 {
+  if(plaintext.empty() || nonce_data.empty()) {
+    return false;
+  }
   /// \todo Encrypt plaintext using AES-GCM with the given nonce and additional data.
-  return false;
+  this->ciphertext.resize(plaintext.size() + additional_data.size());
+
+  // Galois Multiplication
+  // Append each gmult to this->tag
+  // gmult(vector this->tag, vector data_to_mult)
+  gmult(this->tag, additional_data);
+  std::vector<uint8_t> e_counter_zero;
+  this->aes128.encrypt(e_counter_zero, nonce_data);
+
+  // AES GMC
+  // TODO: for loop to go block size 128 rather than vector's indexes
+  for(unsigned int i = 0; i < plaintext.size(); i++) {
+    // increase counter
+    nonce_data++;
+    // encrypt the counter with our key
+    this->aes128.encrypt(ciphertext[i], nonce_data);
+    // ciphertext = encrypted_counter XOR plaintext
+    ciphertext[i] ^= plaintext[i];
+    // tag = collection of ciphertexts XOR with previous tag
+    gmult(this->tag, ciphertext[i] ^ this->tag);
+  }
+  // gmult last element of tag = hash of concatenated additional_data and ciphertext size
+  std::vector<uint8_t> sizes;
+  sizes.push_back(this->ciphertext.size());
+  sizes.push_back(additional_data.size());
+  gmult(this->tag, tag.back() ^ sizes.data());
+  // last element in tag is (gmult(last_tag_element XOR ciphertext_size)) XOR encrypted_zero_counter
+  this->tag.back() ^= e_counter_zero;
+
+  return true;
 }
 
 bool aes128gcm::decrypt(std::vector<uint8_t>& plaintext, const std::vector<uint8_t>& ciphertext,
@@ -36,5 +92,7 @@ bool aes128gcm::decrypt(std::vector<uint8_t>& plaintext, const std::vector<uint8
                         const std::vector<uint8_t>& additional_data) const
 {
   /// \todo Decrypt ciphertext using AEs-GCM with the given nonce and additional data.
-  return false;
+  bool res = encrypt(plaintext, ciphertext, nonce_data, additional_data);
+
+  return res;
 }
