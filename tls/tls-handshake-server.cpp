276158ad9158faff927532c917ee05279c6e3c59
Samirah Amadu <amadu@student.tugraz.at>
2018-11-27 22:34:28 +0100

    Add read client hello

--- tls/tls-handshake-server.cpp
+++ tls/tls-handshake-server.cpp
@@ -1,49 +1,136 @@
 #include "tls-handshake-server.h"
+#include <cstring>
+#include "endian.h"
+#include "hkdf.h"
+#include "hmac-sha2.h"
 
 
-tls_handshake_server::tls_handshake_server(tls_record_layer& layer, const psk_map& psks)
-  : layer_(layer), psks_(psks), ecdh_(SECP256R1), fixed_randomness_({{0}}),
-    have_fixed_randomness_(false)
-{
+tls_handshake_server::tls_handshake_server(tls_record_layer &layer, const psk_map &psks)
+    : layer_(layer), psks_(psks), ecdh_(SECP256R1), fixed_randomness_({{0}}),
+      have_fixed_randomness_(false) {
   ecdh_.generate_key_pair();
 }
 
-tls_handshake_server::tls_handshake_server(tls_record_layer& layer, const psk_map& psks,
-                                           const random_struct& fixed_randomness,
-                                           const gfp_t& ecdh_private)
-  : layer_(layer), psks_(psks), ecdh_(SECP256R1), fixed_randomness_(fixed_randomness),
-    have_fixed_randomness_(true)
-{
+tls_handshake_server::tls_handshake_server(tls_record_layer &layer, const psk_map &psks,
+                                           const random_struct &fixed_randomness,
+                                           const gfp_t &ecdh_private)
+    : layer_(layer), psks_(psks), ecdh_(SECP256R1), fixed_randomness_(fixed_randomness),
+      have_fixed_randomness_(true) {
   ecdh_.set_private_key(ecdh_private);
 }
 
+alert_location tls_handshake_server::process_extensions(std::vector<uint8_t> extensions) {
+  if (extensions.size() < 2) { // minimal length is 2 bytes
+    return {local, handshake_failure};
+  }
 
-alert_location tls_handshake_server::read_client_hello()
-{
-  /// \todo read the ClientHello message from the record layer and handle it
-  return {local, internal_error};
+  uint16_t index = 0;
+  while ((extensions.size() - index + 1) > 0) {
+    uint16_t code = make_uint16(extensions[index], extensions[index + 1]);
+    index += 2;
+    uint16_t size = ntoh(make_uint16(extensions[index], extensions[index + 1]));
+    index += 2;
+
+    std::vector<uint8_t> data(extensions.begin() + index, (extensions.begin() + index + size));
+
+    Extension deserializedExt;
+    deserializedExt.type = static_cast<ExtensionType>(code);
+    deserializedExt.data = data;
+    index += size;
+    received.extentions.push_back(deserializedExt);
+  }
+
+  return {local, ok};
 }
 
-void tls_handshake_server::send_server_hello()
-{
+alert_location tls_handshake_server::read_client_hello() {
+  /// \todo read the ClientHello message from the record layer and handle it
+  std::vector<uint8_t> data;
+
+  alert_location
+      alert = layer_.read(TLS_HANDSHAKE, data, sizeof(handshake_message_header), true); // should it really block?
+
+  if (alert) {
+    return {local, handshake_failure};
+  }
+
+  size_t index = 0;
+
+  // check legacy protocol version
+  uint8_t major = data[index++];
+  uint8_t minor = data[index++];
+  if (major!=TLSv1_2_MAJOR || minor!=TLSv1_2_MINOR) {
+    return {local, handshake_failure};
+  }
+  received.legacy_version = make_uint16(major, minor);
+
+  // get the random number
+  random_struct random;
+  std::vector<uint8_t> vector_rand_section(&data[index],&data[index+32]);
+  memcpy(&random, vector_rand_section.data(), 32);
+  received.random = random;
+  index += 32;
+
+  // process the session id
+  uint8_t legacy_session_id_length(ntoh(data[index]));
+  if (legacy_session_id_length > 32) {
+    return {local, handshake_failure};
+  }
+
+  index += legacy_session_id_length; //used by Versions of TLS before TLS 1.3, therefore no need to process
+
+  //check cipher suites that are supported
+  uint8_t cipher_suite_length = ntoh(data[index]);
+  if (cipher_suite_length < 2 || cipher_suite_length > (sizeof(uint16_t)*16 - 2)) {
+    return {local, handshake_failure};
+  }
+
+  cipher_suite supported = {data[index++], data[index++]};
+  if (supported!=TLS_AES_128_GCM_SHA256 && supported!=TLS_ASCON_128_SHA256) {
+    return {local, handshake_failure}; //check if error correct
+  }
+  received.cipher_suites = {};
+
+  uint8_t legacy_compression_methods_length = ntoh(data[index++]);
+  // If the client is using TLS 1.2 the compression handling needs to be processed
+  // TLS 1.3 can only receive a 1 byte entry with value 0
+
+  if (legacy_compression_methods_length!=1 ) {
+    return {local, illegal_parameter};
+  }
+
+  uint8_t legacy_compression_methods = ntoh(data[index++]);
+  if (legacy_compression_methods!=0) {
+    return {local, illegal_parameter};
+  }
+
+  std::vector<uint8_t> extensions(data.begin() + index, data.end());
+  return process_extensions(extensions);
+}
+
+void tls_handshake_server::send_server_hello() {
 /// \todo write the ServerHello message to the record layer
 /// If have_fixed_randomness_ is false, generate random data.
 /// If it is true, use fixed_randomness_ as random data.
+
+  struct protocol_version legacy_version = TLSv1_2;
+
+
+
+
+
 }
 
-void tls_handshake_server::send_finished()
-{
+void tls_handshake_server::send_finished() {
 /// \todo write the Finished message to the record layer
 }
 
-alert_location tls_handshake_server::read_finished()
-{
+alert_location tls_handshake_server::read_finished() {
   /// \todo read the Finished message from the record layer and handle it
   return {local, internal_error};
 }
 
-alert_location tls_handshake_server::answer_handshake()
-{
+alert_location tls_handshake_server::answer_handshake() {
   // read ClientHello
   alert_location alert = read_client_hello();
   if (!alert)
--- tls/tls-handshake-server.h
+++ tls/tls-handshake-server.h
@@ -7,34 +7,50 @@
 #include "ecdh.h"
 #include "tls-record-layer.h"
 #include "tls.h"
+#include "hkdf.h"
 
 /// Server side of the handshake
-class tls_handshake_server
-{
-public:
+class tls_handshake_server {
+ public:
   /// Instantiate new server-side handshake.
-  tls_handshake_server(tls_record_layer& layer, const psk_map& psks);
+  tls_handshake_server(tls_record_layer &layer, const psk_map &psks);
   /// Instantiate new server-side handshake with fixed randomness and fixed ECDH private key.
-  tls_handshake_server(tls_record_layer& layer, const psk_map& psks,
-                       const random_struct& fixed_randomness, const gfp_t& ecdh_private);
+  tls_handshake_server(tls_record_layer &layer, const psk_map &psks,
+                       const random_struct &fixed_randomness, const gfp_t &ecdh_private);
 
   /// Run the server side of the handshake.
   alert_location answer_handshake();
 
-private:
+ private:
   alert_location read_client_hello();
+  alert_location process_extensions(std::vector<uint8_t> extensions);
+
   void send_server_hello();
   void send_server_hello_done();
   alert_location read_client_key_exchange();
   alert_location read_finished();
   void send_finished();
 
-  tls_record_layer& layer_;
+  tls_record_layer &layer_;
   const psk_map psks_;
 
   ecdh ecdh_;
   const random_struct fixed_randomness_;
   const bool have_fixed_randomness_;
+
+  HandshakePayload received;
+  //hkdf hkdf_hashing;
+
+  /**
+   * From https://github.com/randombit/botan
+   * Make a uint16_t from two bytes
+   * @param i0 the first byte
+   * @param i1 the second byte
+   * @return i0 || i1
+   */
+  uint16_t make_uint16(uint8_t i0, uint8_t i1) {
+    return static_cast<uint16_t>((static_cast<uint16_t>(i0) << 8) | i1);
+  }
 
 };
 
